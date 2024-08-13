91835fc8b496066d4cbd0018bfc47f33bf111a06
Dewi Safira <dewisafira@student.gunadarma.ac.id>
2024-08-13 23:46:18 +0700

    first init github

---
+++ app/Views/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Latihan Ci4</title>
+</head>
+
+<body>
+  <h1>roblok</h1>
+  <img src="https://tr.rbxcdn.com/30DAY-Avatar-8BF0EE9E258EB1F8846E79F00C54F08D-Png/420/420/Avatar/Webp/noFilter"alt="">
+
+</body>
+
+</html>
